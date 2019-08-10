2be51da9be070902d8f3ac399df8d4deb19ac2be
jace lin <734136032@qq.com>
2019-08-10 14:18:19 +0800

    增加RAM缓存文件、空间描述区，加快数据读写速度；增加线程锁适配操作系统、多线程的情况

--- fs_kernel/jacefs.h
+++ fs_kernel/jacefs.h
@@ -18,15 +18,42 @@ typedef enum{
 
 typedef uint16_t jacefs_file_id_t;
 
+#define FS_INVALID_FS_ID (0xffff)
 
+
+/*
+ *  FS使用RAM作空间信息和文件描述区的缓存
+ */
+#define FS_USE_RAM_CACHE (1)
+#if FS_USE_RAM_CACHE
+    void jacefs_sync(void);
+#else
+    #define jacefs_sync()
+#endif
+
+/*
+ *  FS互斥锁，如果使用多线程则需要打开
+ */
+#define FS_USE_LOCK (0)
+
+/*
+ *  FS操作接口
+ */
 jacefs_error_t jacefs_init(void);
 jacefs_error_t jacefs_create(jacefs_file_id_t *file_id,int size,uint16_t app_id);
 jacefs_error_t jacefs_delete(jacefs_file_id_t file_id,uint16_t app_id);
-jacefs_error_t jacefs_delete_by_appid(uint16_t app_id);
+jacefs_error_t jacefs_delete_all(uint16_t app_id,jacefs_file_id_t except_file_id);
+jacefs_error_t jacefs_clean_all(void);
+
 int jacefs_append(jacefs_file_id_t file_id,uint16_t app_id,uint8_t *dat,int size);
 int jacefs_write(jacefs_file_id_t file_id,uint16_t app_id,uint8_t *dat,int size,int offset);
 int jacefs_read(jacefs_file_id_t file_id,uint16_t app_id,uint8_t *dat,int size,int offset);
 
+jacefs_error_t jacefs_find_app_id(jacefs_file_id_t file_id,uint16_t *app_id,int *offset);
+
+/*
+ *  FS调试接口
+ */
 void jacefs_self_test(void);
 
 
